b4073746dee9dd15e122a51e4e5d477a201c78d3
leobortolini <neijoleo@gmail.com>
2019-05-24 17:31:14 -0300

    f
    f

--- src/app/pages/modal-aposta/modal-aposta.page.ts
+++ src/app/pages/modal-aposta/modal-aposta.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-modal-aposta',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModalApostaPage implements OnInit {
 
-  constructor() { }
+  constructor(private ModalApostaController: ModalController) { }
 
   ngOnInit() {
+  }
+
+  apostar() {
+    this.ModalApostaController.dismiss();
+  }
+
+  cancelar() {
+    this.ModalApostaController.dismiss();
   }
 
 }
