a0c85f3eb41eca5e672982394e28bd3311d1cbeb
leobortolini <neijoleo@gmail.com>
2019-06-11 10:55:32 -0300

    sa
    sa

--- src/app/base.service.ts
+++ src/app/base.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Usuario } from './shared/classe.usuario';
 
 @Injectable()
 export class BaseService {
@@ -8,8 +9,11 @@ export class BaseService {
     showUserButton = false;
     showMenuButton = false;
     showBackButton = false;
+    apostou1 = false;
+    apostou2 = false;
     aviso = 'null';
     caminhoImagemPadrao = '../assets/img/';
     jogo2 = true;
     baseURL = 'http://127.0.0.1:8000/mycup';
+    usuario: Usuario = null;
 }
